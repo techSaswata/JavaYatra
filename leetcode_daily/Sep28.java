a96ebe642cb9841596d3610a703ee4c23be6af82
techSaswata <saswata.24bcs10248@sst.scaler.com>
2025-09-28 05:33:55 +0530

    sep28

---
+++ leetcode_daily/Sep28.java
@@ -0,0 +1,15 @@
+package leetcode_daily;
+//lc 976
+import java.util.*;
+
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = nums.length - 1; i >= 2; i--) {
+            if (nums[i - 2] + nums[i - 1] > nums[i]) {
+                return nums[i - 2] + nums[i - 1] + nums[i];
+            }
+        }
+        return 0;
+    }
+}

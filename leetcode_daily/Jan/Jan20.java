05cb8e2e76ecd91db6b4136219cf0bb2257be968
techSaswata <saswata.24bcs10248@sst.scaler.com>
2026-01-20 03:16:42 +0000

    lc 3314

---
+++ leetcode_daily/Jan/Jan20.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+        int i = 0;
+        for (int num : nums) {
+            if (num == 2) {
+                ans[i++] = -1;
+            } else {
+                ans[i++] = num - (1 << (Integer.numberOfTrailingZeros(~num) - 1));
+            }
+        }
+        return ans;
+    }
+}

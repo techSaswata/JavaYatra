c48db0447e102724287e91d87b40ec46f434289d
techSaswata <saswata.24bcs10248@sst.scaler.com>
2026-03-22 03:57:02 +0000

    Determine Whether Matrix Can Be Obtained By Rotation

---
+++ leetcode_daily/2026-Mar/Mar22.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean findRotation(int[][] mat, int[][] target) {
+        int n = mat.length;
+        boolean c0 = true, c90 = true, c180 = true, c270 = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (c0 && mat[i][j] != target[i][j]) c0 = false;
+                if (c90 && mat[i][j] != target[j][n - 1 - i]) c90 = false;
+                if (c180 && mat[i][j] != target[n - 1 - i][n - 1 - j]) c180 = false;
+                if (c270 && mat[i][j] != target[n - 1 - j][i]) c270 = false;
+                if (!c0 && !c90 && !c180 && !c270) return false;
+            }
+        }
+        return true;
+    }
+}

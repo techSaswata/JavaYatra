49a20ed26226a48434dd94be6a9a04be8d776cca
techSaswata <saswata.24bcs10248@sst.scaler.com>
2026-05-23 04:55:28 +0000

    Check if Array Is Sorted and Rotated

---
+++ leetcode_daily/2026-May/May23.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean check(int[] nums) {
+        int count = 0;
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] > nums[i + 1]) {
+                count++;
+                if (count > 1) {
+                    return false;
+                }
+            }
+        }
+        if (nums[n - 1] > nums[0]) {
+            count++;
+            if (count > 1) {
+                return false;
+            }
+        }
+        return true;
+    }
+}

244f7e54cbcbbfa627685ccd2ca9857f5b4d0e82
techSaswata <saswata.24bcs10248@sst.scaler.com>
2026-06-08 06:11:48 +0000

    Partition Array According to Given Pivot

---
+++ leetcode_daily/2026-Jun/Jun08.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] ans = new int[n];
+        int left = 0;
+        int right = n - 1;
+        
+        for (int i = 0, j = n - 1; i < n; i++, j--) {
+            if (nums[i] < pivot) {
+                ans[left++] = nums[i];
+            }
+            if (nums[j] > pivot) {
+                ans[right--] = nums[j];
+            }
+        }
+        
+        while (left <= right) {
+            ans[left++] = pivot;
+        }
+        
+        return ans;
+    }
+}

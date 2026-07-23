6cde018a6f76e5af207e57f016f1b31e7b35bc93
techSaswata <saswata.24bcs10248@sst.scaler.com>
2026-07-23 04:45:39 +0000

    Number of Unique XOR Triplets I

---
+++ leetcode_daily/2026-Jul/Jul23.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+        return Integer.highestOneBit(n) << 1;
+    }
+}

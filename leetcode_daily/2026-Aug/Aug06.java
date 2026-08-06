b3d8162586f847e39413597dfc1d75fddc44a4b1
techSaswata <saswata.24bcs10248@sst.scaler.com>
2026-08-06 04:37:31 +0000

    Smallest Divisible Digit Product I

---
+++ leetcode_daily/2026-Aug/Aug06.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int smallestNumber(int n, int t) {
+        for (int i = n; ; i++) {
+            int prod = 1;
+            int temp = i;
+            while (temp > 0) {
+                prod *= temp % 10;
+                temp /= 10;
+            }
+            if (prod % t == 0) {
+                return i;
+            }
+        }
+    }
+}
